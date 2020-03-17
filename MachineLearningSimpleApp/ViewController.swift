0214bade9967634368df768b013360410f9fa1c7
Rusen Topcu <rusentopcu@msn.com>
2020-03-17 09:49:18 +0300

    Project create

---
+++ MachineLearningSimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearningSimpleApp
+//
+//  Created by Rusen Topcu on 17.03.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
